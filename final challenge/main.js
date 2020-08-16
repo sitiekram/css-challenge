f242b10948cbc02e0c0f32904e8b72436ed110e8
Ekram Seid <ekramseid4@gmail.com>
2020-08-16 11:47:51 +0200

    Delete main.js

--- final challenge/main.js
+++
@@ -1,6 +0,0 @@
- const navtog=document.querySelector('.nav--toggle');
- const nav=document.querySelector('.nav')
-navtog.addEventListener('click',() =>
-{
- nav.classList.toggle('nav--visibile');
-})
